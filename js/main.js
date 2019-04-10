b1b5637edc8917d9d9932ea66bdca98559c6c0d9
Sarah H. Whitmore <sarahwhitmore15@gmail.com>
2019-04-10 19:29:29 -0400

    small align changes before Sarah ruins everything w/ sonogram vid

--- js/main.js
+++ js/main.js
@@ -7,7 +7,27 @@ $(document).ready(function(){
 	var pinIntroScene = new ScrollMagic.Scene({
 		triggerElement: '#intro',
 		triggerHook: 0,
-		duration: '100%'
+		duration: '200%'
+	})
+	.setPin('#intro', {pushFollowers: false})
+	.addTo(controller);
+
+	var controller = new ScrollMagic.Controller();
+
+	// pin the intro
+	var pinIntroScene2 = new ScrollMagic.Scene({
+		triggerElement: '#intro',
+		triggerHook: 0,
+		duration: '200%'
+	})
+	.setPin('#intro', {pushFollowers: false})
+	.addTo(controller);
+
+	// pin the intro
+	var pinIntroScene3 = new ScrollMagic.Scene({
+		triggerElement: '#intro',
+		triggerHook: 0,
+		duration: '200%'
 	})
 	.setPin('#intro', {pushFollowers: false})
 	.addTo(controller);
