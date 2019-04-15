5536c69dcd554b487e330f9d19c19ee82e2f75e0
Sarah H. Whitmore <sarahwhitmore15@gmail.com>
2019-04-15 13:36:25 -0400

    SW

--- js/chart.js
+++ js/chart.js
@@ -2,8 +2,7 @@
 		var ctx = $("#myChart")[0].getContext('2d');
 
 		// DATA
-		var chartData1 = [125, 199, 36, 59, 123, 87];
-		var chartData2 = [35, 99, 126, 139, 103, 177];
+		var chartData1 = [29, 54, 404, 550, 785, 1000];
 
 		// X AXIS LABELS
 		var chartLabelsX = ["Birth", "3 months", "6 months", "9 months", "1 year", "2 years"];
@@ -14,20 +13,20 @@
 			data: {
 				labels: chartLabelsX,
 				datasets: [{
-					label: "Team 1",
 					data: chartData1,
 					fill: false,
 					borderColor: '#c1705e',
 					lineTension: 0
 				},
-				{
+				{	}]
+			},
 			options: {
 				scales: {
 					yAxes: [{
 						ticks: {
 							beginAtZero:true,
 							callback: function (value, index, values) {
-								return '$' + value;
+								return value + 'lbs';
 							}
 						}
 					}]
@@ -39,13 +38,13 @@
 			}
 		});
 
-		var updateChart = function addData(chart, label, data1, data2) {
-			chartLabelsX.push(label);
-			chartData1.push(data1);
-			chartData2.push(data2);
-			//
-			chart.update();
-		};
+		// var updateChart = function addData(chart, label, data1, data2) {
+		// 	chartLabelsX.push(label);
+		// 	chartData1.push(data1);
+		// 	chartData2.push(data2);
+		// 	//
+		// 	chart.update();
+		// };
 
 		// Enable button
 		$('#btn1').on('click', function() {
